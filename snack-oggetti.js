2c8b5f10f35058ef1a326cb6ebb901a054b369de
willymariino <wg.mariino@gmail.com>
2025-07-10 13:54:53 +0200

    first commit

---
+++ snack-oggetti.js
@@ -0,0 +1,13 @@
+
+// 🏆 Code Question 1
+
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name);
+console.log(secondBurger.name);
+
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
