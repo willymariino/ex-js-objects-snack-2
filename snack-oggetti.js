a5c9e0bae8f703c6a14aaf74af28cce659e656b6
willymariino <wg.mariino@gmail.com>
2025-07-10 15:28:32 +0200

    snack - 4

--- snack-oggetti.js
+++ snack-oggetti.js
@@ -120,3 +120,36 @@ Per copiare la prima funzione la tecnica più ideale sarebbe los spread operator
 copiabile solo dallo spread operator, e non si tratta di un oggetto troppo complesso, con un solo un ulteriore livello.
 mentre per la seconda funzione, l'ideale sarebbe structured clone, dato che contiene un tipo di dato speciale.
 */
+
+// 🎯 Code Question 5 (Bonus)
+
+{
+    const hamburger = {
+        name: "Cheese Burger",
+        weight: 250,
+        maker: {
+            name: "Anonymous Chef",
+            restaurant: {
+                name: "Hyur's Burgers",
+                address: "Main Street, 123",
+                isOpen: true,
+            },
+            age: 29
+        }
+    };
+
+    const newRestaurant = { ...hamburger.maker.restaurant };
+    newRestaurant.name = "Hyur's II";
+    newRestaurant.address = "Second Street, 12";
+    const secondBurger = { ...hamburger };
+    secondBurger.maker.restaurant = newRestaurant;
+    secondBurger.maker.name = "Chef Hyur";
+
+    console.log(hamburger.maker.name); // ?
+    console.log(secondBurger.maker.name); // ?
+    console.log(hamburger.maker.restaurant.name); // ?
+    console.log(secondBurger.maker.restaurant.name);
+}
+
+// Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
