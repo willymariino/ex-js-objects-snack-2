55db3945dc5fbe98a7329cc98c0b651d84a23ec2
willymariino <wg.mariino@gmail.com>
2025-07-10 14:07:22 +0200

    snack - 1

--- snack-oggetti.js
+++ snack-oggetti.js
@@ -11,3 +11,14 @@ console.log(secondBurger.name);
 
 // Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+/*
+Risposta:
+
+inizialmente ho l'oggetto hamburger, che successivamente viene salvato dentro la variabile 
+secondBurger, ma solo come riferimento, senza creare copie, quindi l'oggetto continua ad essere uno solo.
+Successivamente vengono modificati i valori name e weight ma andando a modificare l'oggetto originale
+quindi l'oggetto creato è solo 1.
+e visto che sto estraendo solamente il valore name, è l'unica cosa che verrà stampata.
+
+*/
